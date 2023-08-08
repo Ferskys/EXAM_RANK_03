ad5fc212eb61f045c039f24e98d84ba786f7180b
Fernanda Suominsky <fsuomins@c1r1p1.42sp.org.br>
2023-08-08 11:51:35 -0300

    gnl: comentada

--- gnl.c
+++ gnl.c
@@ -1,19 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   gnl.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 10:04:34 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/08/08 10:38:10 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/08/08 11:50:57 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 32
@@ -21,55 +19,66 @@
 
 char *get_next_line(int fd)
 {
-    static char buffer[BUFFER_SIZE];
-    static int index = 0;
-    static int size = 0;
+    static char buffer[BUFFER_SIZE];  // Buffer para armazenar caracteres lidos
+    static int index = 0;             // Índice atual no buffer
+    static int size = 0;              // Número de caracteres lidos no último bloco
 
-    char *line = NULL;
-    int line_size = 0;
-    int line_capacity = 0;
+    char *line = NULL;                // Linha sendo construída
+    int line_size = 0;                // Tamanho atual da linha
+    int line_capacity = 0;            // Capacidade atual da linha
 
-    while (1) {
-        if (index >= size) {
-            index = 0;
-            size = read(fd, buffer, BUFFER_SIZE);
-            if (size < 0) {
-                free(line);
-                return NULL;
-            } else if (size == 0) {
-                break;
+    while (1)  // Loop principal
+	{
+        if (index >= size)  // Verifica se precisa ler um novo bloco
+		{
+            index = 0;  // Reseta o índice para o início do buffer
+            size = read(fd, buffer, BUFFER_SIZE);  // Lê um bloco de caracteres
+
+            if (size < 0)  // Se houver um erro de leitura
+			{
+                free(line);  // Libera a memória alocada para a linha (se houver)
+                return NULL;  // Retorna NULL para indicar um erro
             }
+			else if (size == 0)  // Se o final do arquivo for alcançado
+			    break ;  // Sai do loop
         }
 
-        char current_char = buffer[index++];
-        if (line_size >= line_capacity) {
-            line_capacity += BUFFER_SIZE;
-            char *temp = realloc(line, line_capacity);
-            if (!temp) {
-                free(line);
-                return NULL;
+        char current_char = buffer[index++];  // Lê o caractere atual do buffer
+
+        if (line_size >= line_capacity)  // Verifica se precisa realocar a linha
+		{
+            line_capacity += BUFFER_SIZE;  // Aumenta a capacidade da linha
+            char *temp = realloc(line, line_capacity);  // Realoca a linha
+
+            if (!temp)  // Se a realocação falhar
+			{
+                free(line);  // Libera a memória alocada para a linha
+                return NULL;  // Retorna NULL para indicar um erro
             }
-            line = temp;
+            line = temp;  // Atualiza o ponteiro da linha
         }
 
-        line[line_size++] = current_char;
+        line[line_size++] = current_char;  // Adiciona o caractere à linha
 
-        if (current_char == '\n') {
-            break;
+        if (current_char == '\n')  // Verifica se encontrou uma quebra de linha
+            break ;  // Sai do loop
+    }
+
+    if (line)  // Se a linha não estiver vazia
+	{
+        char *temp = realloc(line, line_size + 1);  // Realoca a linha para o tamanho exato
+
+        if (temp)  // Se a realocação for bem-sucedida
+		{
+            line = temp;             // Atualiza o ponteiro da linha
+            line[line_size] = '\0';  // Adiciona um caractere nulo para terminar a string
         }
     }
-    
-    if (line) {
-        char *temp = realloc(line, line_size + 1); // Redimensionar para o tamanho exato da linha
-        if (temp) {
-            line = temp;
-            line[line_size] = '\0';
-        }
-    }
 
-    return line;
+    return line;  // Retorna a linha lida (ou NULL se houver erro)
 }
 
+/* 
 
 int main()
 {
@@ -90,4 +99,4 @@ int main()
 
     close(fd);
     return 0;
-}
+} */
