3d2a0179f7b2dfd3d537cf0ee63a4c186c71ee3a
Fsuomins <fsuomins@student.42sp.org.br>
2023-10-04 13:17:56 -0300

    wip

--- ft_printf.c
+++ ft_printf.c
@@ -3,77 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 10:55:31 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/08/08 12:02:47 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/10/04 13:12:39 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
+
 #include <unistd.h>
-#include <stdio.h> 
+#include <stdarg.h>
 
-int ft_strlen(const char *str) {
-    int len = 0;
-    while (str[len] != '\0')
-        len++;
-    return len;
+void	put_string(char *string, int *length)
+{
+	if (!string)
+		string = "(null)";
+	while (*string)
+		*length += write(1, string++, 1);
 }
 
-void ft_putstr(char *str) {
-    write(1, str, ft_strlen(str));
+void	put_digit(long long int number, int base, int *length)
+{
+	char	*hexadecimal = "0123456789abcdef";
+
+	if (number < 0)
+	{
+		number *= -1;
+		*length += write(1, "-", 1);
+	}
+	if (number >= base)
+		put_digit((number / base), base, length);
+	*length += write(1, &hexadecimal[number % base], 1);
 }
 
-void ft_putnbr_hex(unsigned int n) {
-    char hex_digits[] = "0123456789abcdef";
-    if (n >= 16)
-        ft_putnbr_hex(n / 16);
-    write(1, &hex_digits[n % 16], 1);
+int	ft_printf(const char *format, ...)
+{
+	int length = 0;
+
+	va_list	pointer;
+	va_start(pointer, format);
+
+	while (*format)
+	{
+		if ((*format == '%') && ((*(format + 1) == 's') || (*(format + 1) == 'd') || (*(format + 1) == 'x')))
+		{
+			format++;
+			if (*format == 's')
+				put_string(va_arg(pointer, char *), &length);
+			else if (*format == 'd')
+				put_digit((long long int)va_arg(pointer, int), 10, &length);
+			else if (*format == 'x')
+				put_digit((long long int)va_arg(pointer, unsigned int), 16, &length);
+		}
+		else
+			length += write(1, format, 1);
+		format++;
+	}
+	return (va_end(pointer), length);
 }
 
-int ft_printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    int written = 0;
-    int i = 0;
-    while (format[i] != '\0') {
-        if (format[i] == '%' && format[i + 1] != '\0') {
-            i++;
-            if (format[i] == 's') {
-                char *str = va_arg(args, char *);
-                ft_putstr(str);
-                written += ft_strlen(str);
-            } else if (format[i] == 'd') {
-                int num = va_arg(args, int);
-                char num_str[12]; // Assuming a maximum of 11 digits for an int
-                int len = snprintf(num_str, sizeof(num_str), "%d", num);
-                write(1, num_str, len);
-                written += len;
-            } else if (format[i] == 'x') {
-                unsigned int num = va_arg(args, unsigned int);
-                ft_putnbr_hex(num);
-                written += sizeof(unsigned int) * 2; // Each hex digit is 4 bits
-            } else {
-                // Unsupported conversion specifier, just print the character
-                write(1, &format[i], 1);
-                written++;
-            }
-        } else {
-            write(1, &format[i], 1);
-            written++;
-        }
-        i++;
-    }
-
-    va_end(args);
-    return written;
-}
-
-int main() {
+/* int main() {
     ft_printf("%s\n", "toto");
     ft_printf("Magic %s is %d\n", "number", 42);
     ft_printf("Hexadecimal for %d is %x\n", 42, 42);
     return 0;
 }
+ */
---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 13:10:40 by fsuomins          #+#    #+#             */
+/*   Updated: 2023/10/04 13:10:59 by fsuomins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
